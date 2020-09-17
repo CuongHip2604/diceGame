5b5f06fbf9222844146d5f74baa1d2803fdb695a
Cuong Nguyen <cuong.nguyen@savvycomsoftware.com>
2020-09-17 12:59:45 +0700

    deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/diceGame'));
+app.use(express.static(__dirname + '/dist/dice-game'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/diceGame/index.html'));
+res.sendFile(path.join(__dirname+'/dist/dice-game/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
